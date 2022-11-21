502efcfef00a5ca462868350a7c351aa56c3744d
jobin <jobinjose.in@gmail.com>
2022-11-21 21:13:31 +0530

    Basic structure added

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        while(true) {
+            System.out.println("------------------ Menu ----------------------");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Search Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Exit");
+            System.out.println("----------------------------------------------");
+            System.out.print("\nPlease enter the choice: ");
+            int choice = sc.nextInt();
+
+            }
+        }
+    }
+}
